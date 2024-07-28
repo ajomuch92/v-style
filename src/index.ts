5d30a8bb5b82cd6f3a81077fa1127cea130f40e4
Aarón J. Montes <aaronmontes_92@yahoo.es>
2024-07-28 15:26:20 -0600

    feat: 🎸 Creating main directive body

---
+++ src/index.ts
@@ -0,0 +1,73 @@
+import { App, Directive } from 'vue';
+import { stringToHash, toKebabCase } from './utils';
+
+const map = new Map();
+
+function addScriptTag(el: any, value: any) {
+    const styles = Object.entries(value).reduce(
+        (previusValue, currentValue) => {
+            const [key, value] = currentValue;
+            if (value)
+                return `${previusValue}${toKebabCase(key)}: ${value};\n`;
+            return previusValue;
+        },
+        '',
+    );
+
+    const hash = stringToHash(styles);
+    if (map.has(hash)) {
+      const count = map.get(hash) as number;
+      map.set(hash, count + 1);
+    } else {
+      const className = `v-style-${stringToHash(styles)}`;
+
+      const styleElement = document.createElement('style');
+      styleElement.dataset.className = className;
+
+      styleElement.innerHTML = `.${className} { ${styles} }`;
+
+      document.head.appendChild(styleElement);
+
+      el.classList.add(className);
+      el.setAttribute('data-custom-class', className);
+      map.set(hash, 1);
+    }
+}
+
+function removeScriptTag(el: any) {
+  const className: string | undefined | null = el.getAttribute('data-custom-class');
+    if (className) {
+        const hash = className.split('-')[2];
+        const styleElement = document.querySelector(
+            `style[data-class-name="${className}"]`,
+        );
+        if (styleElement) {
+            styleElement.parentNode.removeChild(styleElement);
+        }
+        el.classList.remove(className);
+        if (map.get(hash) === 1) {
+          map.delete(hash);
+        } else {
+          map.set(hash, map.get(hash) - 1);
+        }
+    }
+}
+
+export const vStyle: Directive = {
+  created(el, binding) {
+    const { value } = binding;
+    addScriptTag(el, value)
+  },
+  updated(el, binding) {
+    const { value } = binding;
+    removeScriptTag(el);
+    addScriptTag(el, value);
+  },
+  beforeUnmount(el) {
+    removeScriptTag(el);
+  }
+};
+
+export default function install(app: App) {
+  app.directive('style', vStyle);
+}
